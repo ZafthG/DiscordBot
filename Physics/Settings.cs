389016bc1b2ff96d5624fef72fc9172d3b36df58
Gabriel Ferreira <112124836+ZafthG@users.noreply.github.com>
2022-10-05 20:10:38 -0300

    Update Settings.cs

--- Physics/Settings.cs
+++ Physics/Settings.cs
@@ -13,23 +13,21 @@ namespace Physics
         /// <summary> Delimita o máximo de tentativas antes de definir uma falha. </summary>
         public const int Services_MaxTries = 5;
         /// <summary> Tempo padrão para execução das atividades de atualização (em minutos). </summary>
-        public const int Default_TimeUpdate = 1;
+        public const int Default_TimeUpdate = 60;
         /// <summary> Horário padrão para os alertas do bot. </summary>
         public const int Alert_StandardHour = 7;
 
         /// <summary> Servidor MySQL. </summary>
-        public const string MySQL_Server = "";
+        public const string MySQL_Server = "localhost";
         /// <summary> Usuário do MySQL. </summary>
-        public const string MySQL_Uid = "";
+        public const string MySQL_Uid = "root";
         /// <summary> Senha do MySQL. </summary>
-        public const string MySQL_Pwd = "";
+        public const string MySQL_Pwd = "admin123";
         /// <summary> Nome do banco de dados MySQL. </summary>
-        public const string MySQL_Database = "";
-        /// <summary> Limite de tempo para a conexão ficar ausente (em minutos). </summary>
-        public const int MySQL_Timeout = 10;
+        public const string MySQL_Database = "botdb";
 
         /// <summary> Token de conexão para o bot do Discord. </summary>
-        public const string Discord_BotToken = "";
+        public const string Discord_BotToken = "MTAwOTEzOTA5NTA4NTI1Njc5Ng.G7gL6-.SldzvRP42OTiC59bDIOK2Zoolvp98PWAtzxvDY";
         /// <summary> ID do bot dentro do sistema do Discord. </summary>
         public const ulong Discord_BotId = 1009139095085256796;
 
